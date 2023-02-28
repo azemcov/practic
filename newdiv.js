279a15058bdf97e7c6f220a7fa3eca82404459eb
azemcov <111581518+azemcov@users.noreply.github.com>
2023-02-28 20:54:30 +0300

    Add files via upload

---
+++ newdiv.js
@@ -0,0 +1,18 @@
+{
+    //создаём кнопку
+    let newButton = document.createElement('button')
+    newButton.id='button'
+    newButton.innerHTML='Добавить элемент'
+    document.getElementById('section').append(newButton)
+    //
+    let i=1;
+    let a = document.getElementById('button').onclick = function () {
+        let newDiv = document.createElement('div');
+        newDiv.id = `div${i}`;
+        newDiv.innerHTML = 'ffff';
+        document.getElementById('button').after(newDiv);
+        i++;
+        i++;
+        i++;
+    }
+}
