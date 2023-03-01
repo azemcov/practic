74c7f767c151c63d075d549e60e9b37a2027641d
azemcov <111581518+azemcov@users.noreply.github.com>
2023-03-01 10:42:18 +0500

    пробелы

--- newdiv.js
+++ newdiv.js
@@ -1,16 +1,16 @@
 {
-    //создаём кнопку
-    let newButton = document.createElement('button')
-    newButton.id='button'
-    newButton.innerHTML='Добавить элемент'
-    document.getElementById('section').append(newButton)
-    //
-    let i=1;
-    let a = document.getElementById('button').onclick = function () {
-        let newDiv = document.createElement('div');
-        newDiv.id = `div${i}`;
-        newDiv.innerHTML = 'dddd';
-        document.getElementById('button').after(newDiv);
-        i++;
-    }
+  //создаём кнопку
+  let newButton = document.createElement('button')
+  newButton.id='button'
+  newButton.innerHTML='Добавить элемент'
+  document.getElementById('section').append(newButton)
+  //
+  let i=1;
+  let a = document.getElementById('button').onclick = function () {
+      let newDiv = document.createElement('div');
+      newDiv.id = `div${i}`;
+      newDiv.innerHTML = 'dddd';
+      document.getElementById('button').after(newDiv);
+      i++;
+  }
 }
