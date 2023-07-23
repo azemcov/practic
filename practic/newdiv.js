8f39cdf6bfc6ee149a1396ed45364afe264aa01a
Alexandr Zemtsov <a.zemcov@gmail.com>
2023-07-23 16:50:51 +0500

    название изменений

--- practic/newdiv.js
+++ practic/newdiv.js
@@ -1,6 +1,6 @@
 function newDiv(n,i){
-  let newDiv = document.createElement('div');
-  newDiv.id = n;
-  newDiv.innerHTML=i;
-  document.getElementById('section').append(newDiv);
+  let newD = document.createElement('div');
+  newD.id = n;
+  newD.innerHTML=i;
+  document.getElementById('section').append(newD);
 }
